fff945d82c16bb75e2b7edec34e9ce87bca8ef05
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-04-30 22:05:28 +0800

    add component stock item

---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css']
+})
+export class StockItemComponent implements OnInit {
+
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+  public positiveChange: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
+  }
+
+}

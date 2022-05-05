4c33a97ad97cf823f2a68b9c1b1cf12386ddbf95
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-05-05 11:36:10 +0800

    test

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,17 +9,25 @@ import { Stock } from 'src/app/model/stock';
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
+  public stocks: Array<Stock>;
+  public stockStyles;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765)
+    ];
   }
 
-  toggleFavorite(event) {
-    console.log('We are toggling the favorite state for this stock', event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event, index) {
+    console.log('We are toggling the favorite state for this stock', index, event);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 
+  trackStockByCode(index, stock) {
+    return stock.code;
+  }
 }

51e47f49294d3ad215ac301c91f8a7b0071f63b2
INFOR\dsun1 <dalei.sun@infor.com>
2022-05-07 11:56:02 +0800

    input

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Stock } from 'src/app/model/stock';
 
@@ -9,25 +9,16 @@ import { Stock } from 'src/app/model/stock';
 })
 export class StockItemComponent implements OnInit {
 
-  public stocks: Array<Stock>;
-  public stockStyles;
+  @Input() public stock: Stock;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.stocks = [
-      new Stock('Test Stock Company', 'TSC', 85, 80),
-      new Stock('Second Stock Company', 'SSC', 10, 20),
-      new Stock('Last Stock Company', 'LSC', 876, 765)
-    ];
+      
   }
 
-  toggleFavorite(event, index) {
-    console.log('We are toggling the favorite state for this stock', index, event);
-    this.stocks[index].favorite = !this.stocks[index].favorite;
-  }
-
-  trackStockByCode(index, stock) {
-    return stock.code;
+  toggleFavorite(event) {
+    console.log('We are toggling the favorite state for this stock', event);
+    this.stock.favorite = !this.stock.favorite;
   }
 }

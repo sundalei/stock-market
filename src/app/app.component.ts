07d69762df7d09f65e49ecd4dbad2580560b2ba6
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-05-08 23:26:40 +0800

    change detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,30 @@ export class AppComponent implements OnInit {
   title = 'stock-market';
 
   public stockObj: Stock;
+  private counter: number = 1;
 
   ngOnInit(): void {
-      this.stockObj = new Stock('Test Stock Company', 'TSC', 85, 80);
+      this.stockObj = new Stock('Test Stock Company - ' + this.counter++, 'TSC', 85, 80);
+  }
+
+  onToggleFavorite(stock: Stock) {
+    // This will update the value in the stock item component
+    // Because it is triggered as a result of an event
+    // binding from the stock item component
+    console.log('Favorite for stock ', stock, ' was triggered');
+    this.stockObj.favorite = !this.stockObj.favorite;
+  }
+
+  changeStockObject() {
+    // This will update the value in the stock item component
+    // Because we are creating a new reference for the stock input
+    this.stockObj = new Stock('Test Stock Company - ' + this.counter++, 'TSC', 85, 80);
+  }
+
+  changeStockPrice() {
+    // This will not update the value in the stock item component
+    // Because it is changing the same reference and angular will 
+    // not check for it in the OnPush change detection strategy.
+    this.stockObj.price += 10;
   }
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,24 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { Stock } from 'src/app/model/stock';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.css']
+  styleUrls: ['./stock-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StockItemComponent implements OnInit {
 
   @Input() public stock: Stock;
+  @Output() private toggleFavorite: EventEmitter<Stock>;
 
-  constructor() { }
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
+  }
 
   ngOnInit(): void {
       
   }
 
-  toggleFavorite(event) {
-    console.log('We are toggling the favorite state for this stock', event);
-    this.stock.favorite = !this.stock.favorite;
+  onToggleFavorite(event) {
+    this.toggleFavorite.emit(this.stock);
+  }
+
+  changeStockPrice() {
+    this.stock.price += 5;
   }
 }
